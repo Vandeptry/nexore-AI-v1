40e28ac5b0468a92ab5a5d06d7a5da40841848f0
Vandeptry <emangafromnowhere@gmail.com>
2025-03-28 01:37:23 +0700

    Update recognizer.py

--- recognizer.py
+++ recognizer.py
@@ -24,15 +24,16 @@ def recognize_face(img_path: str, db_path: str, threshold=0.65, min_avg_score=0.
     avg_scores.sort(key=lambda x: x[1], reverse=True)
 
     best_name, best_score = avg_scores[0]
+
     if best_score >= min_avg_score:
-    return {
-        "status": "success",
-        "name": best_name,
-        "score": float(best_score) 
-    }
-else:
-    return {
-        "status": "low_confidence",
-        "name": best_name,
-        "score": float(best_score) 
-    }
+        return {
+            "status": "success",
+            "name": best_name,
+            "score": float(best_score)  # ✅ ép kiểu ở đây
+        }
+    else:
+        return {
+            "status": "low_confidence",
+            "name": best_name,
+            "score": float(best_score)
+        }
